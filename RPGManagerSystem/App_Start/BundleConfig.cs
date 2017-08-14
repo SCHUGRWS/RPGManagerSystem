556930c3f1230cd1c4c29bdb86275387ec6af1b6
Richard Wilhian Schug <richard.schug@outlook.com>
2017-08-14 19:58:29 -0300

    Primeiro Commit

--- RPGManagerSystem/App_Start/BundleConfig.cs
+++ RPGManagerSystem/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace RPGManagerSystem
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/estiloGeral.css",
                       "~/Content/site.css"));
         }
     }
---
+++ RPGManagerSystem/Controllers/JogadoresController.cs
@@ -0,0 +1,19 @@
+﻿using RPGManagerSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RPGManagerSystem.Controllers
+{
+    public class JogadoresController : Controller
+    {
+        // GET: Jogadores
+        public ActionResult Index()
+        {
+            var jogador = new JogadoresModel() { Nome = "Richard Wilhian Schug" };
+            return View(jogador);
+        }
+    }
+}
